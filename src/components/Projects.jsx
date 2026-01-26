f53edb57f1f0974d5e68b5371f1e9cc0b57be418
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-27 01:17:00 +0530

    Update projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
+import { ArrowUpRight } from 'lucide-react'
 
 const projects = [
   {
-    img: "/comingsoon.png",
+    img: "/comingsoon.jpg",
     title: "Coming Soon",
     desc: "This project is currenlty under construction, and will be launching soon",
-    stack: ["React","Typescript", "TankStack Query"],
+    stack: ["React", "Typescript", "TankStack Query"],
     demo: "https://example.com",
     github: "https://github.com/naayann"
   },
   {
-    img: "/comingsoon.png",
+    img: "/comingsoon.jpg",
     title: "Coming Soon",
     desc: "This project is currenlty under construction, and will be launching soon",
-    stack: ["React","Typescript", "TankStack Query"],
+    stack: ["React", "Typescript", "TankStack Query"],
     demo: "https://example.com",
     github: "https://github.com/naayann"
   },
   {
-    img: "/comingsoon.png",
+    img: "/comingsoon.jpg",
     title: "Coming Soon",
     desc: "This project is currenlty under construction, and will be launching soon",
-    stack: ["React","Typescript", "TankStack Query"],
+    stack: ["React", "Typescript", "TankStack Query"],
     demo: "https://example.com",
     github: "https://github.com/naayann"
   },
   {
-    img: "/comingsoon.png",
+    img: "/comingsoon.jpg",
     title: "Coming Soon",
     desc: "This project is currenlty under construction, and will be launching soon",
-    stack: ["React","Typescript", "TankStack Query"],
+    stack: ["React", "Typescript", "TankStack Query"],
     demo: "https://example.com",
     github: "https://github.com/naayann"
   },
@@ -38,21 +39,41 @@ const projects = [
 const Projects = () => {
   return (
     <>
-    <h1 className='mt-4 mb-4 font-bold text-xl'>Projects</h1>
-      <div className='grid grid-cols-2 gap-2'>
+      <h1 className='mt-4 mb-4 font-bold text-xl'>Projects</h1>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
         {projects.map((project, i) => (
-          <div key={i} className='border border-gray-500 rounded-2xl p-3'>
-            <img src={project.img} alt="coming-soon" className='rounded-lg'/>
-            <div className='flex justify-between'>
-              <h2 className='text-xl font-bold'>{project.title}</h2>
+          <div
+            key={i}
+            className='border border-gray-500 rounded-2xl p-3'>
+
+            <img
+              src={project.img}
+              alt="coming-soon"
+              className='rounded-lg grayscale hover:grayscale-0 transition'
+            />
+
+            <div className='flex justify-between py-2'>
+
+              <h2 className='text-md font-bold'>{project.title}</h2>
               <p className='flex items-center gap-1'>
-                <div className='w-3 h-3 rounded-full bg-green-500 animate-pulse'/>
+                <div className='w-3 h-3 rounded-full bg-green-500 animate-pulse' />
                 Building
               </p>
             </div>
-            <p>{project.desc}</p>
-            <div>
+            <p
+              className='text-sm text-gray-500 tracking-wider'>
+              {project.desc}
+            </p>
 
+            <div className='flex gap-1 mt-2'>
+              {project.stack.map((tech, iTech) => (
+                <span
+                  key={iTech}
+                  className='gap-2 border border-gray-500 rounded-full px-2 text-xs text-gray-300'>
+                  {tech}
+                </span>
+              ))}
             </div>
           </div>
         ))}
