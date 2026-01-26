a093b8519698311363d6fe2072d23e2b7a31e8af
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-26 11:58:35 +0530

    Replace navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import LoadingScreen from './components/LoadingScreen'
-import NavBar from './components/NavBar'
+import NavBar2 from './components/NavBar2'
 import HomeFeed from './components/HomeFeed'
 import Projects from './components/Projects'
+import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
 
@@ -22,11 +23,12 @@ function App() {
         <>
           <div className="container w-200 mx-auto">
             <div>
-              <NavBar />
+              <NavBar2 />
             </div>
             <div className='flex flex-col'>
               <HomeFeed />
               <Projects />
+              <Skills />
               <Experience />
               <Contact />
             </div>
