409e126c411824edad96c26ea2b0c916b3960e1a
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-27 20:17:40 +0530

    Update profile picture path

--- src/components/sections/HomeFeed.jsx
+++ src/components/sections/HomeFeed.jsx
@@ -1,5 +1,4 @@
 import { Download, Send } from 'lucide-react'
-import profile from "/profile.jpg"
 
 const HomeFeed = () => {
   return (
@@ -27,7 +26,7 @@ const HomeFeed = () => {
       <div className="-mt-10">
         <div className="flex items-end gap-4">
           <img
-            src={profile}
+            src="/profile.jpg"
             alt="Nayan Sarania"
             className="w-28 h-28 rounded-2xl ring-1 ring-white/20 hover:ring-white/50 transition-all duration-200 p-1 z-50 bg-white/5"
           />
